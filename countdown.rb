036bdfd63fa477512d0100f8d5653c5d27b954bc
Kristen Warnquist <kristen.warnquist@gmail.com>
2017-12-03 19:42:08 +0000

    automatically backed up by learn

--- countdown.rb
+++ countdown.rb
@@ -1,7 +1,11 @@
 #write your code here
 
 def countdown
-  puts "#{number} SECOND(S)!"
+  number = 10
+  while number > 0
+    puts "#{number} SECOND(S)!"
+    number -= 1
+  end
 end
 
 puts "HAPPY NEW YEAR!"
